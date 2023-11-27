77de897713bae1eb17a76809cb530f657bda3b5f
Alexis Ambray <alexisambray@gmail.com>
2023-11-28 04:37:48 +0800

    Enhanced code to align with project specifications

--- src/cache_simulation.py
+++ src/cache_simulation.py
@@ -1,37 +1,39 @@
 class CacheSimulator:
-    def __init__(self, cache_size, block_size):
-        if block_size >= cache_size:
-          raise ValueError("Block size should be less than cache size.")
-      
-        self.cache_size = cache_size
-        self.block_size = block_size
+    def __init__(self):
+        # Fixed cache size and block size as per instructions
+        self.cache_size = 32
+        self.block_size = 64
 
-        # Define the cache line size in words (adjust based on your requirements)
+        # Fixed cache line size in words as per instructions
         self.cache_line_size = 64
 
         # Calculate the number of cache blocks and sets
-        self.num_blocks = cache_size // block_size
-        self.num_sets = max(1, self.num_blocks // 4)
-        print(f"num_blocks: {self.num_blocks}, num_sets: {self.num_sets}")
+        self.num_blocks = max(self.cache_size // self.block_size, 1)  # Ensure num_blocks is at least 1
+        self.num_sets = max(self.num_blocks // 4, 1)  # Ensure num_sets is at least 1
 
         # Calculate the number of words per block based on the cache line size
-        self.num_words_per_block = block_size // self.cache_line_size
+        self.num_words_per_block = self.block_size // self.cache_line_size
 
         # Initialize the cache as a list of sets, each containing 4 lines (blocks)
-        self.cache = [[{'valid': False, 'tag': None, 'data': [None] * self.num_words_per_block} for _ in range(4)] for _ in range(self.num_sets)]
+        self.cache = [
+            [{'valid': False, 'tag': None, 'data': [None] * self.num_words_per_block} for _ in range(4)]
+            for _ in range(self.num_sets)
+        ]
 
         # LRU counters for each set
         self.lru_counters = [[0] * 4 for _ in range(self.num_sets)]
-        
-    def run_simulation(self, memory_access_sequence):
-        hits, misses = 0, 0
-        cache_access_time = 1  # Placeholder for cache access time
-        main_memory_access_time = 10  # Placeholder for main memory access time
 
+        # Variables to store the cache memory trace
+        self.cache_memory_trace = []
+
+        # Variables to store simulation results
+        self.hits = 0
+        self.misses = 0
+
+    def run_simulation(self, memory_access_sequence):
         for address in memory_access_sequence:
             block_address, offset = divmod(address, self.block_size)
             set_index = (block_address // self.block_size) % self.num_sets
-            print(f"block_address: {block_address}, num_sets: {self.num_sets}, set_index: {set_index}")
             tag = block_address // self.num_blocks
 
             # Check each line in the set for a hit
@@ -39,7 +41,7 @@ class CacheSimulator:
             for i in range(4):
                 if self.cache[set_index][i]['valid'] and self.cache[set_index][i]['tag'] == tag:
                     # Cache hit
-                    hits += 1
+                    self.hits += 1
                     hit = True
                     # Update LRU counters
                     self.update_lru_counters(set_index, i)
@@ -47,7 +49,7 @@ class CacheSimulator:
 
             if not hit:
                 # Cache miss
-                misses += 1
+                self.misses += 1
                 # Find the least recently used line in the set
                 lru_index = self.get_lru_index(set_index)
                 # Replace the least recently used line with the new block
@@ -55,19 +57,19 @@ class CacheSimulator:
                 # Update LRU counters
                 self.update_lru_counters(set_index, lru_index)
 
-                # Load data from main memory (placeholder for load-through policy)
-                # You need to implement the logic to load data into the cache
+            # Log the cache memory state for each memory access
+            self.cache_memory_trace.append([line.copy() for line in self.cache])
 
         # Calculate cache hit rate, cache miss rate, average memory access time, and total memory access time
-        cache_hit_rate = hits / len(memory_access_sequence)
-        cache_miss_rate = misses / len(memory_access_sequence)
-        average_memory_access_time = cache_hit_rate * cache_access_time + cache_miss_rate * main_memory_access_time
+        cache_hit_rate = self.hits / len(memory_access_sequence)
+        cache_miss_rate = self.misses / len(memory_access_sequence)
+        average_memory_access_time = cache_hit_rate * 1 + cache_miss_rate * 10  # Placeholder values for access times
         total_memory_access_time = len(memory_access_sequence) * average_memory_access_time
 
-        # Return statistics
+        # Return simulation results
         return {
-            'hits': hits,
-            'misses': misses,
+            'hits': self.hits,
+            'misses': self.misses,
             'cache_hit_rate': cache_hit_rate,
             'cache_miss_rate': cache_miss_rate,
             'average_memory_access_time': average_memory_access_time,
