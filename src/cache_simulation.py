cbfb42c97f0e0f48a3270cf4f98d24487408fa7c
Alexis Ambray <alexisambray@gmail.com>
2023-11-29 05:15:59 +0800

    Fixed run simulation with test cases

--- src/cache_simulation.py
+++ src/cache_simulation.py
@@ -29,8 +29,8 @@ class CacheSimulator:
         # Variables to store simulation results
         self.hits = 0
         self.misses = 0
-
-    def run_simulation(self, memory_access_sequence):
+        
+    def run_simulation(self, memory_access_sequence, output_option='final_snapshot'):
         for address in memory_access_sequence:
             block_address, offset = divmod(address, self.block_size)
             set_index = (block_address // self.block_size) % self.num_sets
@@ -66,15 +66,47 @@ class CacheSimulator:
         average_memory_access_time = cache_hit_rate * 1 + cache_miss_rate * 10  # Placeholder values for access times
         total_memory_access_time = len(memory_access_sequence) * average_memory_access_time
 
-        # Return simulation results
-        return {
-            'hits': self.hits,
-            'misses': self.misses,
-            'cache_hit_rate': cache_hit_rate,
-            'cache_miss_rate': cache_miss_rate,
-            'average_memory_access_time': average_memory_access_time,
-            'total_memory_access_time': total_memory_access_time
-        }
+        # Return simulation results and memory trace
+        if output_option == 'final_snapshot':
+            return {
+                'hits': self.hits,
+                'misses': self.misses,
+                'cache_hit_rate': cache_hit_rate,
+                'cache_miss_rate': cache_miss_rate,
+                'average_memory_access_time': average_memory_access_time,
+                'total_memory_access_time': total_memory_access_time,
+                'memory_trace': self.cache_memory_trace[-1]  # Return the final snapshot
+            }
+        elif output_option == 'step_by_step':
+            return {
+                'hits': self.hits,
+                'misses': self.misses,
+                'cache_hit_rate': cache_hit_rate,
+                'cache_miss_rate': cache_miss_rate,
+                'average_memory_access_time': average_memory_access_time,
+                'total_memory_access_time': total_memory_access_time,
+                'memory_trace': self.cache_memory_trace  # Return the full memory trace for step-by-step tracing
+            }
+        else:
+            raise ValueError("Invalid output option. Choose 'final_snapshot' or 'step_by_step'")
+
+    def handle_cache_miss(self, set_index, tag, block_address):
+        # Fetch data from main memory (simulate load-through)
+        data = self.fetch_data_from_memory(block_address)
+
+        # Find the least recently used line in the set
+        lru_index = self.get_lru_index(set_index)
+
+        # Update cache with the fetched data
+        self.cache[set_index][lru_index] = {'valid': True, 'tag': tag, 'data': data}
+
+        # Update LRU counters
+        self.update_lru_counters(set_index, lru_index)
+
+    def fetch_data_from_memory(self, block_address):
+        # Simulate fetching data from main memory
+        # This can be replaced with actual memory access logic
+        return [None] * self.num_words_per_block
 
     def update_lru_counters(self, set_index, accessed_line):
         # Increment the counter of the accessed line and reset others in the set
